947c7fcd70c74ebc5844e57fe19ab82e5e5c32a2
William Park <galaxiapark07@gmail.com>
2026-02-10 01:50:10 +1100

    does vcd output default back to ns every time? last re-run and might have to crop the time entirely in the powerpoint then

--- ugl_am_radio/fpga/formal/wd_cover/engine_0/trace3_tb.v
+++ ugl_am_radio/fpga/formal/wd_cover/engine_0/trace3_tb.v
@@ -9,16 +9,16 @@ module testbench(input clock, output reg genclock);
   reg genclock = 1;
   reg [31:0] cycle = 0;
   reg [0:0] PI_force_reset;
-  reg [0:0] PI_enable;
+  reg [0:0] PI_rstn;
   reg [0:0] PI_heartbeat;
   wire [0:0] PI_clk = clock;
-  reg [0:0] PI_rstn;
+  reg [0:0] PI_enable;
   watchdog_timer UUT (
     .force_reset(PI_force_reset),
-    .enable(PI_enable),
+    .rstn(PI_rstn),
     .heartbeat(PI_heartbeat),
     .clk(PI_clk),
-    .rstn(PI_rstn)
+    .enable(PI_enable)
   );
 `ifndef VERILATOR
   initial begin
@@ -54,81 +54,81 @@ module testbench(input clock, output reg genclock);
 
     // state 0
     PI_force_reset = 1'b0;
-    PI_enable = 1'b1;
-    PI_heartbeat = 1'b0;
     PI_rstn = 1'b0;
+    PI_heartbeat = 1'b0;
+    PI_enable = 1'b1;
   end
   always @(posedge clock) begin
     // state 1
     if (cycle == 0) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     // state 2
     if (cycle == 1) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     // state 3
     if (cycle == 2) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     // state 4
     if (cycle == 3) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     // state 5
     if (cycle == 4) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     // state 6
     if (cycle == 5) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     // state 7
     if (cycle == 6) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b0;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b0;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b0;
     end
 
     // state 8
     if (cycle == 7) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b0;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b0;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b0;
     end
 
     // state 9
     if (cycle == 8) begin
       PI_force_reset <= 1'b0;
-      PI_enable <= 1'b1;
-      PI_heartbeat <= 1'b0;
       PI_rstn <= 1'b1;
+      PI_heartbeat <= 1'b0;
+      PI_enable <= 1'b1;
     end
 
     genclock <= cycle < 9;
